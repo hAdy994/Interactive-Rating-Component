3f27f9e701183fe62456cc1ed90779c84a468110
hAdy994 <hady_awayda@hotmail.com>
2023-03-23 08:40:20 +0200

    Initial Commit

---
+++ README-template.md
@@ -0,0 +1,63 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Create Vite App Using React And Typescript
+- Tailwind
+- Chrome and Firefox Dev Tools
+- Flexbox
+- Mobile-first workflow
+- [React](https://react.dev/) - JS library
+- [Next.js](https://vitejs.dev/) - React framework
+
+
+## Author
+
+- Website - [Hady Awayda](https://www.hadyawayda.com)
+- Frontend Mentor - [@hAdy994](https://www.frontendmentor.io/profile/hAdy994)
+
+## Acknowledgments
+
+** As always, thanks to ChatGPT, who has been enabling us developers to create amazing UIs since 2023.
+And also, thanks to web dev cody on youtube https://www.youtube.com/watch?v=mp1-HUjZE0o&ab_channel=WebDevCody for inspiring me to take this challenge.
+
+
---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [selectedRating, setSelectedRating] = useState<number>()
+
+  function handleSubmit() {
+    setIsSubmitted(true)
+  }
+
+  function handleSelectedRating(rating: number) {
+    setSelectedRating(rating)
+  }
+  
+  return (
+    <div className='flex justify-center items-center'>
+      {isSubmitted
+        ? <div className='flex flex-col items-center max-h-fit max-w-sm modal absolute p-8 rounded-3xl'>
+            <div className=' rounded-full p-4 flex max-w-max mb-3.5'>
+              <img src='/illustration-thank-you.svg' width={160} height={160} />
+            </div>
+            <div className='bg-gray-800 text-amber-600 p-1 px-4 mb-6 rounded-3xl text-base'>
+              You selected {selectedRating} out of 5
+            </div>
+            <div className='flex flex-col items-center'>
+              <h1 className='text-2xl font-medium my-3'>Thank you!</h1>
+              <p className='text-sm leading-relaxed text-center'>We appreciate you taking the time to give a rating.</p>
+              <p className='text-sm leading-relaxed text-center'>If you ever need more support, don't hesitate to get in touch!</p>
+            </div>
+          </div>
+        : <form onSubmit={handleSubmit} className='flex flex-col max-h-fit max-w-sm modal absolute p-8 rounded-3xl'>
+            <div className='bg-gray-700 rounded-full p-4 flex max-w-max mb-2'>
+              <img src='/icon-star.svg' width={18} height={18} />
+            </div>
+            <div>
+              <div className='mb-6'>
+                <h1 className='text-2xl font-medium my-4'>How did we do?</h1>
+                <p className='text-base leading-relaxed'>Please let us know how we did with your support request. All feedback is appreciated 
+                to help us improve our offering!</p>
+              </div>
+          </div>
+          <div className='flex justify-between mb-6'>
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <button onClick={() => handleSelectedRating(rating)} type='button' className='h-12 w-12 rounded-full background bg-gray-700 hover:bg-slate-500 hover:text-white focus:bg-amber-600 focus:text-white transition-colors duration-500'>{rating}</button>
+            ))}
+            </div>
+          <div className='flex justify-center items-center'>
+            <button disabled={selectedRating === undefined} className='flex grow items-center h-12 justify-center rounded-full text-xl bg-amber-600 hover:bg-white hover:text-amber-600 transition-colors duration-300'>Submit</button>
+            </div>
+        </form>}
+    </div>
+  )
+}
+
+export default App;
