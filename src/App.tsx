557256e9367c33da5b3e0592dd81946bab0d8674
hAdy994 <hady_awayda@hotmail.com>
2023-03-24 23:03:56 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import thanks from '/illustration-thank-you.svg'
+import star from '/icon-star.svg'
 
 function App() {
   const [isSubmitted, setIsSubmitted] = useState(false)
@@ -14,35 +16,34 @@ function App() {
   }
   
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-center h-screen items-center'>
       {isSubmitted
-        ? <div className='flex flex-col items-center max-h-fit max-w-sm modal absolute p-8 rounded-3xl'>
+        ? <div className='flex flex-col items-center max-h-fit max-w-sm modal absolute p-8 rounded-3xl mx-4'>
             <div className=' rounded-full p-4 flex max-w-max mb-3.5'>
-              <img src='/Interactive-Rating-Component/illustration-thank-you.svg' width={160} height={160} />
+              <img src={thanks} width={160} height={160} />
             </div>
             <div className='bg-gray-800 text-amber-600 p-1 px-4 mb-6 rounded-3xl text-base'>
               You selected {selectedRating} out of 5
             </div>
             <div className='flex flex-col items-center'>
               <h1 className='text-2xl font-medium my-3'>Thank you!</h1>
-              <p className='text-sm leading-relaxed text-center'>We appreciate you taking the time to give a rating.</p>
-              <p className='text-sm leading-relaxed text-center'>If you ever need more support, don't hesitate to get in touch!</p>
+              <p className='text-sm leading-relaxed text-center'>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
             </div>
           </div>
-        : <form onSubmit={handleSubmit} className='flex flex-col max-h-fit max-w-sm modal absolute p-8 rounded-3xl'>
-            <div className='bg-gray-700 rounded-full p-4 flex max-w-max mb-4'>
-              <img src='/Interactive-Rating-Component/icon-star.svg' width={18} height={18} />
+        : <form onSubmit={handleSubmit} className='flex flex-col max-h-fit modal absolute p-8 rounded-3xl mx-4'>
+            <div className='rounded-full p-4 flex max-w-max mb-7 background'>
+              <img src={star} width={16} height={16} />
             </div>
             <div>
-              <div className='mb-6'>
-                <h1 className='text-2xl font-medium my-4'>How did we do?</h1>
-                <p className='text-base leading-relaxed'>Please let us know how we did with your support request. All feedback is appreciated 
+              <div className='mb-8'>
+                <h1 className='text-3xl font-medium my-2.5'>How did we do?</h1>
+                <p className='text-sm leading-relaxed'>Please let us know how we did with your support request. All feedback is appreciated 
                 to help us improve our offering!</p>
               </div>
           </div>
-          <div className='flex justify-between mb-6'>
+          <div className='flex justify-between mb-8'>
             {[1, 2, 3, 4, 5].map((rating) => (
-              <button onClick={() => handleSelectedRating(rating)} type='button' className='h-12 w-12 rounded-full background bg-gray-700 hover:bg-slate-500 hover:text-white focus:bg-amber-600 focus:text-white transition-colors duration-500'>{rating}</button>
+              <button onClick={() => handleSelectedRating(rating)} type='button' className='h-12 w-12 rounded-full hover:text-white focus:text-white transition-colors duration-500 background'>{rating}</button>
             ))}
             </div>
           <div className='flex justify-center items-center'>
